aed4c063271f1c82a81281a764b1c8314f20da3d
Jonny <djonli2003@gmail.com>
2022-02-10 19:02:57 +0200

    Ready project

--- tic-tac-toe/Form1.cs
+++ tic-tac-toe/Form1.cs
@@ -12,11 +12,41 @@ namespace tic_tac_toe
 {
     public partial class ticTacToe : Form
     {
+        private int player;
+        private int x = 12,
+                    y = 12;
+        private Button[,] buttons = new Button[3, 3];
+        
         public ticTacToe()
         {
             InitializeComponent();
             this.Height = 700;
             this.Width = 900;
+            player = 1;
+
+            for (int i = 0; i < buttons.Length / 3; i++)
+            { 
+                for (int j = 0; j < buttons.Length / 3; j++)
+                { 
+                    buttons[i, j] = new Button();
+                    buttons[i, j].Size = new Size(200, 200);
+                }
+            }
+            SetButtons();
+        }
+
+        private void SetButtons()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    buttons[i, j].Location = new Point(12 + 206 * i, 12 + 206 * j);
+                    buttons[i, j].Click += button1_Click;
+                    buttons[i, j].Font = new Font(new FontFamily("Microsoft Sans Serif"), 138);
+                    this.Controls.Add(buttons[i, j]);
+                }
+            }
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -26,8 +56,105 @@ namespace tic_tac_toe
 
         private void button1_Click(object sender, EventArgs e)
         {
-            //MessageBox.Show(sender.GetType().GetProperty("Name").GetValue(sender).ToString());
-            sender.GetType().GetProperty("Text").SetValue(sender, "x");
+            switch (player) 
+            {
+                case 1:
+                    sender.GetType().GetProperty("Text").SetValue(sender, "x");
+                    player = 0;
+                    label1.Text = "Current move: player X";
+                    break;
+
+                case 0:
+                    sender.GetType().GetProperty("Text").SetValue(sender, "o");
+                    player = 1;
+                    label1.Text = "Current move: player O";
+                    break;
+            }
+
+            sender.GetType().GetProperty("Enabled").SetValue(sender, false);
+            CheckWin();
+        }
+
+        private void buttonPlay_Click_1(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    buttons[i, j].Text = "";
+                    buttons[i, j].Enabled = true;
+                }
+            }
+        }
+
+        private void CheckWin()
+        {
+
+            if (buttons[0, 0].Text == buttons[0, 1].Text && buttons[0, 1].Text == buttons[0, 2].Text)
+            {
+
+                if (buttons[0, 0].Text != "")
+                {
+                    MessageBox.Show("You win");
+                    return;
+                }
+            }
+            if (buttons[1, 0].Text == buttons[1, 1].Text && buttons[1, 1].Text == buttons[1, 2].Text)
+            {
+                if (buttons[1, 0].Text != "")
+                {
+                    MessageBox.Show("You win");
+                    return;
+                }
+            }
+            if (buttons[2, 0].Text == buttons[2, 1].Text && buttons[2, 1].Text == buttons[2, 2].Text)
+            {
+                if (buttons[2, 0].Text != "")
+                {
+                    MessageBox.Show("You win");
+                    return;
+                }
+            }
+            if (buttons[0, 0].Text == buttons[1, 0].Text && buttons[1, 0].Text == buttons[2, 0].Text)
+            {
+                if (buttons[0, 0].Text != "")
+                {
+                    MessageBox.Show("You win");
+                    return;
+                }
+            }
+            if (buttons[0, 1].Text == buttons[1, 1].Text && buttons[1, 1].Text == buttons[2, 1].Text)
+            {
+                if (buttons[0, 1].Text != "")
+                {
+                    MessageBox.Show("You win");
+                    return;
+                }
+            }
+            if (buttons[0, 2].Text == buttons[1, 2].Text && buttons[1, 2].Text == buttons[2, 2].Text)
+            {
+                if (buttons[0, 2].Text != "")
+                {
+                    MessageBox.Show("You win");
+                    return;
+                }
+            }
+            if (buttons[0, 0].Text == buttons[1, 1].Text && buttons[1, 1].Text == buttons[2, 2].Text)
+            {
+                if (buttons[0, 0].Text != "")
+                {
+                    MessageBox.Show("You win");
+                    return;
+                }
+            }
+            if (buttons[2, 0].Text == buttons[1, 1].Text && buttons[1, 1].Text == buttons[0, 2].Text)
+            {
+                if (buttons[2, 0].Text != "")
+                {
+                    MessageBox.Show("You win");
+                    return;
+                }
+            }
         }
     }
 }
